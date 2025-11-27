2ce580dcce464611f61523628d7ed20cebcd321d
spacedrone404 <81930707+spacedrone404@users.noreply.github.com>
2025-11-27 18:52:26 +0300

    huge update

--- readme.md
+++ readme.md
@@ -1,40 +1,52 @@
 # Basic dummy stub for the arcade game
 
-Demo version. No mobile layout at this time.
+Demo version. Incomplete functionality.
 <br>
-Intended to be used on desktop & laptop.
+Intended to be used on desktop & laptop. Limited mobile use.
 <br>
-Control Dangerous Dave in wacky space riding green glowing blob by means of arrow keys.
+Control Dangerous Dave in wacky space riding green glowing blob by means of:
+
+- [▲] [▼] [◄] [►] arrow keys for movement
+- [Spacebar] for laser shot
+- [F8] to turn on/off orbiting planets
+- [Enter] to become as cool as Duke Nukem
+
+Features:
+
+- no heavy frameworks and dependencies, just pure JavaScript
+- compact size, less than 500Kb including graphics and audio
+- tiny core only 35Kb!
+- completely free, no license, do with it whatever you what
+
 <br>
 <br>
 More stuff to come (why we have only 24h per day?).
 
 ## GFX niceties
 
-- double backdrop, second one is moving and rotating with transparency
-- random blinking and moving stars upon startup
-- "raining" moving stars from top to bottom
+- multitude of space backdrops
+- static stars [layer1], moving and rotating with transparency [layer2]
+- random blinking [layer3] and moving stars [layer4]
+- "raining" moving stars from top to bottom [layer5]
 
 ## Todo plans
 
-- scale Dave when he is going up/down to show perspective
-- make Dave draggable by mouse
 - improve performance if it is possible
-- implement mobile version
 
 ## Done stuff
 
+- beta mobile [touch] support
 - add sounds to startup, actions and backdrop ambience
 - make Dave leave trace while moving
 - implement space bar fire
-- make Dave to turn arround to the direction he is heading to
+- make Dave to turn around to the direction he is heading to
 - random starting location of blinking stars, they also should change their formation upon blinking
 
 ## Technical details
 
 Ultra low-budget i3-3220 from 2013 runs it quite decently, but i already see the limit of the configuration.
 <br>
-Turn off some planet orbits [or turn them off completely] in markup to improve performance by a big margin.
+Turn off some orbiting planets to improve performance by a big margin.
 
 Written in plain HTML / CSS / JavaScript
 <br>
@@ -43,3 +55,7 @@ OS: Windows 7 x64 ESU
 Editor: VsCodium 1.97
 <br>
 Browser: RedFox 144
+
+## In memory of my mom
+
+† Rest in peace, maybe it is even better place than ours
--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ let soundPlayed = false; // Var for audio muting
 let lastFacingDirection = -1; // default facing position
 
 document.addEventListener("keydown", function (event) {
-  // event.preventDefault();
+  event.preventDefault();
 
   switch (event.code) {
     case "ArrowLeft":
@@ -63,6 +63,13 @@ document.addEventListener("keydown", function (event) {
       playSoundBlast("media/audio/blast.mp3");
       shootBullet();
       break;
+    case "Enter":
+      showSpeechBubble(
+        dave,
+        "IT'S TIME TO KICK ASS AND CHEW BUBBLEGUM & I'AM ALL OUT OF GUM!"
+      );
+      playSoundMove("media/audio/duke3d/duke3d.mp3");
+      break;
   }
 });
 
@@ -89,13 +96,17 @@ document.addEventListener("keyup", function (event) {
   }
 });
 
+// Turns Dave right
 function updateDaveTransformRight() {
   dave.style.transform = "scaleX(-1) translateX(-35px) translateY(-100px)";
 }
 
+// Turns Dave left
 function updateDaveTransformLeft() {
   dave.style.transform = "scaleX(1) translateX(-35px) translateY(-100px)";
 }
+
+// Dave movement
 
 function updatePosition() {
   // acceleration
@@ -159,10 +170,14 @@ function renderTrail() {
     trailEl.style.mixBlendMode = "hard-light";
 
     const colorIndex = Math.min(index / maxTrailLength, 1);
-    const r = Math.round(Math.max(0, 255 - colorIndex * 55));
-    const g = Math.round(colorIndex * 255);
+    const r = Math.round(Math.max(0, 255 - colorIndex * 155));
+    const g = Math.round(colorIndex * 155);
     const b = Math.round(colorIndex * 155);
-    trailEl.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    trailEl.style.backgroundImage = `radial-gradient(circle at center, rgb(${r},${g},${b}), transparent 100%)`;
+    trailEl.style.boxShadow = `
+      inset 0 0 0 4px orange,
+      0 0 0 4px orange
+    `;
 
     wrapper.appendChild(trailEl);
   });
@@ -181,22 +196,29 @@ function shootBullet() {
   bulletImg.style.position = "absolute";
   bulletImg.style.left = "0px";
   bulletImg.style.top = "0px";
+  bulletImg.style.transform = "scale(2.24)";
+  bulletImg.style.imageRendering = "pixelated";
 
   if (directionX === -1) {
-    bulletImg.style.transform = "scaleX(-1)";
+    // bulletImg.style.transform = "scaleX(-1)";
   }
 
   bulletsContainer.appendChild(bulletImg);
 
   setTimeout(() => {
-    bulletImg.style.transition = "left 0.5s linear";
-    bulletImg.style.left = `${directionX * 512}px`; // blast distance
+    bulletImg.style.transition = "all 0.5s linear";
+    bulletImg.style.left = `${directionX * 512}px`;
   }, 100);
 
-  // blast removal
   setTimeout(() => {
-    bulletsContainer.removeChild(bulletImg);
-  }, 600);
+    bulletImg.style.transition = "all 0.8s linear";
+    bulletImg.style.filter = "blur(10px)";
+    bulletImg.style.opacity = "0";
+
+    bulletImg.addEventListener("transitionend", () => {
+      bulletsContainer.removeChild(bulletImg);
+    });
+  }, 800);
 
   // blast audio
   playSoundBlast("media/audio/blast.mp3");
@@ -220,8 +242,14 @@ playSoundStartup();
 // teleport startup sound
 function playSoundStartup(src) {
   const audioStartup = new Audio("media/audio/teleport.mp3");
-  audioStartup.volume = 0.3;
+  const dukeStartup = new Audio("media/audio/duke3d/duke3d-intro.mp3");
+  audioStartup.volume = 0.4;
   audioStartup.play();
+
+  setTimeout(function () {
+    dukeStartup.volume = 1;
+    dukeStartup.play();
+  }, 2000);
 }
 
 // backdrop ambience sound
@@ -234,3 +262,149 @@ function playSoundAmbience() {
 }
 
 playSoundAmbience();
+
+// Dave's speech bubble
+
+function showSpeechBubble(element, text) {
+  const bubble = document.createElement("div");
+  bubble.className = "speech-bubble";
+  bubble.textContent = text;
+
+  const rect = element.getBoundingClientRect();
+  bubble.style.left = `${rect.right + 20}px`;
+  bubble.style.top = `${rect.top - 20}px`;
+
+  setTimeout(() => {
+    bubble.classList.add("fade-in");
+  }, 100);
+
+  setTimeout(() => {
+    bubble.classList.add("fade-out");
+    setTimeout(() => bubble.remove(), 800);
+  }, 4000);
+
+  document.body.appendChild(bubble);
+}
+
+// touch input support
+let startTouchPos = { x: null, y: null };
+
+document.addEventListener("touchstart", function (event) {
+  event.preventDefault();
+  startTouchPos.x = event.touches[0].clientX;
+  startTouchPos.y = event.touches[0].clientY;
+});
+
+document.addEventListener("touchmove", function (event) {
+  event.preventDefault();
+  const moveDeltaX = event.touches[0].clientX - startTouchPos.x;
+  const moveDeltaY = event.touches[0].clientY - startTouchPos.y;
+
+  if (Math.abs(moveDeltaX) > Math.abs(moveDeltaY)) {
+    if (moveDeltaX > 0) {
+      isRightPressed = true;
+      isLeftPressed = false;
+      updateDaveTransformRight();
+    } else {
+      isLeftPressed = true;
+      isRightPressed = false;
+      updateDaveTransformLeft();
+    }
+  } else {
+    if (moveDeltaY > 0) {
+      isDownPressed = true;
+      isUpPressed = false;
+    } else {
+      isUpPressed = true;
+      isDownPressed = false;
+    }
+  }
+
+  if (!soundPlayed) {
+    playSoundMove("media/audio/move.mp3");
+    soundPlayed = true;
+  }
+});
+
+document.addEventListener("touchend", function (event) {
+  event.preventDefault();
+  isLeftPressed = false;
+  isRightPressed = false;
+  isUpPressed = false;
+  isDownPressed = false;
+  soundPlayed = false;
+});
+
+// secondary touch
+let firstTouchID = null;
+
+document.addEventListener("touchstart", function (event) {
+  event.preventDefault();
+
+  if (firstTouchID === null) {
+    firstTouchID = event.touches[0].identifier;
+    startTouchPos.x = event.touches[0].clientX;
+    startTouchPos.y = event.touches[0].clientY;
+  } else {
+    if (event.touches.length >= 2) {
+      shootBullet();
+    }
+  }
+});
+
+document.addEventListener("touchmove", function (event) {
+  event.preventDefault();
+
+  if (firstTouchID !== null) {
+    const activeTouch = Array.from(event.touches).find(
+      (touch) => touch.identifier === firstTouchID
+    );
+
+    if (activeTouch) {
+      const moveDeltaX = activeTouch.clientX - startTouchPos.x;
+      const moveDeltaY = activeTouch.clientY - startTouchPos.y;
+
+      if (Math.abs(moveDeltaX) > Math.abs(moveDeltaY)) {
+        if (moveDeltaX > 0) {
+          isRightPressed = true;
+          isLeftPressed = false;
+          updateDaveTransformRight();
+        } else {
+          isLeftPressed = true;
+          isRightPressed = false;
+          updateDaveTransformLeft();
+        }
+      } else {
+        if (moveDeltaY > 0) {
+          isDownPressed = true;
+          isUpPressed = false;
+        } else {
+          isUpPressed = true;
+          isDownPressed = false;
+        }
+      }
+
+      if (!soundPlayed) {
+        playSoundMove("media/audio/move.mp3");
+        soundPlayed = true;
+      }
+    }
+  }
+});
+
+document.addEventListener("touchend", function (event) {
+  event.preventDefault();
+
+  // removal of first touch if it is not completed
+  if (
+    firstTouchID !== null &&
+    event.changedTouches.some((touch) => touch.identifier === firstTouchID)
+  ) {
+    firstTouchID = null;
+    isLeftPressed = false;
+    isRightPressed = false;
+    isUpPressed = false;
+    isDownPressed = false;
+    soundPlayed = false;
+  }
+});
